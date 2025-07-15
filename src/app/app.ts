1ece8e0eef782868a36211e3929ded222dced82c
Gopi P <pgopirmd@gmail.com>
2025-07-15 08:51:50 +0530

    code changes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
-import { Hello } from '../hello/hello';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Hello],
+  imports: [FormsModule, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   protected title = 'first';
+  name: string = 'Angular';
+  fruits = ['Apple', 'orange'];
+  isLoggedId = true;
 }
