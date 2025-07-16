f89a95474f3f187bdff0db1c8603100ec9d6cfc9
Gopi P <pgopirmd@gmail.com>
2025-07-16 08:54:16 +0530

    output input

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,15 +2,22 @@ import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { Hello } from '../hello/hello';
 @Component({
   selector: 'app-root',
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, Hello],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   protected title = 'first';
-  name: string = 'Angular';
-  fruits = ['Apple', 'orange'];
-  isLoggedId = true;
+  parentmessage: string = 'This is from parent';
+  // name: string = 'Angular';
+  // fruits = ['Apple', 'orange'];
+  // isLoggedId = true;
+  // value: string = '40';
+
+  handleReceivedData(data: string) {
+    console.log(data);
+  }
 }
--- src/hello/hello.ts
+++ src/hello/hello.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
-import { SecondComponent } from '../second-component/second-component';
+import {
+  Component,
+  EventEmitter,
+  Output,
+  output,
+  input,
+  Input,
+} from '@angular/core';
 
 @Component({
   selector: 'app-hello',
-  imports: [SecondComponent],
+  imports: [],
   templateUrl: './hello.html',
   styleUrl: './hello.css',
 })
-export class Hello {}
+export class Hello {
+  // emiter = output<string>();
+
+  // fromparent = input<string>();
+
+  // older method
+  @Output()
+  emiter: EventEmitter<string> = new EventEmitter();
+
+  @Input() fromparent: string = '';
+
+  emitData() {
+    this.emiter.emit('from child');
+  }
+}
