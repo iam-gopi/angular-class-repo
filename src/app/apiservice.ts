d137f338a5bbf892f3cd705f588dca65a755a731
Gopi P <pgopirmd@gmail.com>
2025-07-22 08:35:29 +0530

    post api

--- src/app/apiservice.ts
+++ src/app/apiservice.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Observable } from 'rxjs';
+import { RegisterData } from './registerdata';
 
 @Injectable({
   providedIn: 'root',
@@ -11,5 +12,17 @@ export class Apiservice {
 
   get(): Observable<any> {
     return this.http.get('https://687dac57918b642243326113.mockapi.io/v1/data');
+  }
+
+  post(data: RegisterData | null): Observable<any> {
+    return this.http.post(
+      'https://687dac57918b642243326113.mockapi.io/v1/data',
+      JSON.stringify(data),
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
   }
 }
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 import { Firstservice } from './firstservice';
 import { Apiservice } from './apiservice';
+import { RegisterData } from './registerdata';
 @Component({
   selector: 'app-root',
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
@@ -14,12 +21,23 @@ export class App {
   protected title = 'first';
   parentmessage: string = 'This is from parent';
   a: any;
+  myform: FormGroup;
+  formData: RegisterData | null = null;
   // name: string = 'Angular';
   // fruits = ['Apple', 'orange'];
   // isLoggedId = true;
   // value: string = '40';
 
-  constructor(private firstService: Firstservice, private api: Apiservice) {}
+  constructor(
+    private firstService: Firstservice,
+    private api: Apiservice,
+    private fb: FormBuilder
+  ) {
+    this.myform = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
 
   // handleReceivedData(data: string) {
   //   console.log(data);
@@ -31,5 +49,20 @@ export class App {
       console.log(x);
     });
     console.log(this.a);
+  }
+
+  formSubmitHandler() {
+    console.log('from');
+
+    if (this.myform.invalid) {
+      alert(
+        'Form data are not valid. all fields are mandatory and enter proper data'
+      );
+      return;
+    }
+
+    this.formData = this.myform.value;
+
+    this.api.post(this.formData).subscribe((x) => console.log(x));
   }
 }
