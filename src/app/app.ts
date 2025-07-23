d03ef408cc093d03116004445d5546bce384f871
Gopi P <pgopirmd@gmail.com>
2025-07-23 08:30:35 +0530

    optimization

--- src/app/app.ts
+++ src/app/app.ts
@@ -11,58 +11,57 @@ import { RouterOutlet } from '@angular/router';
 import { Firstservice } from './firstservice';
 import { Apiservice } from './apiservice';
 import { RegisterData } from './registerdata';
+import { Hello } from '../hello/hello';
+
+import { NgOptimizedImage } from '@angular/common';
+
 @Component({
   selector: 'app-root',
-  imports: [FormsModule, CommonModule, ReactiveFormsModule],
+  imports: [FormsModule, CommonModule, ReactiveFormsModule, NgOptimizedImage],
   templateUrl: './app.html',
   styleUrl: './app.css',
+  standalone: true,
 })
 export class App {
-  protected title = 'first';
-  parentmessage: string = 'This is from parent';
-  a: any;
-  myform: FormGroup;
-  formData: RegisterData | null = null;
+  a = 'favicon.ico';
+  // protected title = 'first';
+  // parentmessage: string = 'This is from parent';
+  // a: any;
+  // myform: FormGroup;
+  // formData: RegisterData | null = null;
   // name: string = 'Angular';
   // fruits = ['Apple', 'orange'];
   // isLoggedId = true;
   // value: string = '40';
-
-  constructor(
-    private firstService: Firstservice,
-    private api: Apiservice,
-    private fb: FormBuilder
-  ) {
-    this.myform = this.fb.group({
-      name: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-    });
-  }
-
-  // handleReceivedData(data: string) {
-  //   console.log(data);
+  // constructor(
+  //   private firstService: Firstservice,
+  //   private api: Apiservice,
+  //   private fb: FormBuilder
+  // ) {
+  //   this.myform = this.fb.group({
+  //     name: ['', Validators.required],
+  //     email: ['', [Validators.required, Validators.email]],
+  //   });
   // }
-
-  ngOnInit() {
-    // console.log(this.firstService.wel());
-    this.a = this.api.get().subscribe((x) => {
-      console.log(x);
-    });
-    console.log(this.a);
-  }
-
-  formSubmitHandler() {
-    console.log('from');
-
-    if (this.myform.invalid) {
-      alert(
-        'Form data are not valid. all fields are mandatory and enter proper data'
-      );
-      return;
-    }
-
-    this.formData = this.myform.value;
-
-    this.api.post(this.formData).subscribe((x) => console.log(x));
-  }
+  // // handleReceivedData(data: string) {
+  // //   console.log(data);
+  // // }
+  // ngOnInit() {
+  //   // console.log(this.firstService.wel());
+  //   this.a = this.api.get().subscribe((x) => {
+  //     console.log(x);
+  //   });
+  //   console.log(this.a);
+  // }
+  // formSubmitHandler() {
+  //   console.log('from');
+  //   if (this.myform.invalid) {
+  //     alert(
+  //       'Form data are not valid. all fields are mandatory and enter proper data'
+  //     );
+  //     return;
+  //   }
+  //   this.formData = this.myform.value;
+  //   this.api.post(this.formData).subscribe((x) => console.log(x));
+  // }
 }
